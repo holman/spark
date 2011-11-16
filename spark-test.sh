631e282cc440775a5f8cc75220bafad08011a71c
Zach Holman <zach@zachholman.com>
2011-11-15 20:46:11 -0800

    fix tests that #40 broke

--- spark-test.sh
+++ spark-test.sh
@@ -7,28 +7,28 @@ spark="./spark"
 it_graphs_argv_data() {
   graph="$($spark 1,5,22,13,5)"
 
-  test $graph = '▁▂▇▅▂'
+  test $graph = '▁▃█▇▃'
 }
 
 it_charts_pipe_data() {
   data="0,30,55,80,33,150"
   graph="$(echo $data | $spark)"
 
-  test $graph = '▁▂▃▄▂▇'
+  test $graph = '▁▂▄▅▂█'
 }
 
 it_charts_spaced_data() {
   data="0 30 55 80 33 150"
   graph="$($spark $data)"
 
-  test $graph = '▁▂▃▄▂▇'
+  test $graph = '▁▂▄▅▂█'
 }
 
 it_charts_way_spaced_data() {
   data="0 30               55 80 33     150"
   graph="$($spark $data)"
 
-  test $graph = '▁▂▃▄▂▇'
+  test $graph = '▁▂▄▅▂█'
 }
 
 it_handles_decimals() {
@@ -42,21 +42,21 @@ it_charts_100_lt_300() {
   data="1,2,3,4,100,5,10,20,50,300"
   graph="$($spark $data)"
 
-  test $graph = '▁▁▁▁▃▁▁▁▂▇'
+  test $graph = '▁▁▁▁▃▁▁▁▂█'
 }
 
 it_charts_50_lt_100() {
   data="1,50,100"
   graph="$($spark $data)"
 
-  test $graph = '▁▄▇'
+  test $graph = '▁▅█'
 }
 
 it_charts_4_lt_8() {
   data="2,4,8"
   graph="$($spark $data)"
 
-  test $graph = '▂▄▇'
+  test $graph = '▂▄█'
 
 }
 
