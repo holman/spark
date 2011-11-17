986b7fee88b912622876deddc68a21e264d06e29
Zach Holman <zach@zachholman.com>
2011-11-17 13:56:48 -0800

    update tests

--- spark-test.sh
+++ spark-test.sh
@@ -7,35 +7,35 @@ spark="./spark"
 it_graphs_argv_data() {
   graph="$($spark 1,5,22,13,5)"
 
-  test $graph = '▁▃█▇▃'
+  test $graph = '▁▂█▅▂'
 }
 
 it_charts_pipe_data() {
   data="0,30,55,80,33,150"
   graph="$(echo $data | $spark)"
 
-  test $graph = '▁▂▄▅▂█'
+  test $graph = '▁▂▃▄▂█'
 }
 
 it_charts_spaced_data() {
   data="0 30 55 80 33 150"
   graph="$($spark $data)"
 
-  test $graph = '▁▂▄▅▂█'
+  test $graph = '▁▂▃▄▂█'
 }
 
 it_charts_way_spaced_data() {
   data="0 30               55 80 33     150"
   graph="$($spark $data)"
 
-  test $graph = '▁▂▄▅▂█'
+  test $graph = '▁▂▃▄▂█'
 }
 
 it_handles_decimals() {
   data="5.5,20"
   graph="$($spark $data)"
 
-  test $graph = '▃█'
+  test $graph = '▁█'
 }
 
 it_charts_100_lt_300() {
@@ -49,14 +49,14 @@ it_charts_50_lt_100() {
   data="1,50,100"
   graph="$($spark $data)"
 
-  test $graph = '▁▅█'
+  test $graph = '▁▄█'
 }
 
 it_charts_4_lt_8() {
   data="2,4,8"
   graph="$($spark $data)"
 
-  test $graph = '▂▄█'
+  test $graph = '▁▃█'
 
 }
 
@@ -64,6 +64,6 @@ it_charts_no_tier_0() {
   data="1,2,3,4,5"
   graph="$($spark $data)"
 
-  test $graph = '▁▂▃▄▅'
+  test $graph = '▁▂▄▆█'
 
 }
