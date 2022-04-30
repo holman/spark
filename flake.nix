39537c0cd018cc0f421e2c25a89a42bc85763640
Gustavo Porto <gustavo@portosanti.com>
2022-04-30 12:14:04 +0100

    chore: initial flake init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+  };
+}
